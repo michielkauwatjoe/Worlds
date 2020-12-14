f07afd4afa04199acc96120da9e7f7a9ca64a62f
Michiel <michiel@kauwatjoe.com>
2020-12-14 11:49:09 +0100

    Adding Go example.

---
+++ go/hello.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# https://towardsdatascience.com/introduction-to-go-for-python-programmers-a0a729112a1b
+
+go run world.go
---
+++ go/world.go
@@ -0,0 +1,7 @@
+// main package
+package main// package imports
+import "fmt"// main() function
+
+func main() {
+  fmt.Println("Hello world!")
+}
